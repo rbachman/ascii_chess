1396b9f4e91f23fe30179630efe41aafc04d0b0f
Ryan Bachman <ryanbach@amazon.com>
2022-04-14 07:10:31 -0400

    adding cdk skeleton for client

---
+++ client/ascii_chess/ascii_chess_stack copy.py
@@ -0,0 +1,16 @@
+from aws_cdk import (
+    Duration,
+    Stack,
+    aws_lambda_python_alpha as awsPLambda,
+    aws_apigateway as apigw,
+    aws_lambda as awsLambda
+)
+
+from constructs import Construct
+
+
+class AsciiChessStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+    
