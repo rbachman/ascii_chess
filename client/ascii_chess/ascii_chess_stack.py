0d70aaa366fd3d71d421c4f91de1edf828cc0df1
Ryan Bachman <ryanbach@amazon.com>
2022-04-14 07:24:51 -0400

    dependencies

--- client/ascii_chess/ascii_chess_stack.py
+++ client/ascii_chess/ascii_chess_stack.py
@@ -3,7 +3,9 @@ from aws_cdk import (
     Stack,
     aws_lambda_python_alpha as awsPLambda,
     aws_apigateway as apigw,
-    aws_lambda as awsLambda
+    aws_lambda as awsLambda,
+    aws_codecommit as codecommit, 
+    aws_codeartifact as codeartifact
 )
 
 from constructs import Construct
@@ -13,3 +15,4 @@ class AsciiChessStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
